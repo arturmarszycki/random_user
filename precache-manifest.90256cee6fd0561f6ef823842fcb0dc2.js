efb3840978716277f09958f215ecae419e10b6b9
arturmarszycki <artur.marszycki@gmail.com>
2019-10-16 16:40:24 +0200

    Updates

---
+++ precache-manifest.90256cee6fd0561f6ef823842fcb0dc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dae6ff14f9a3c2d44c754289dd21f1d",
+    "url": "/random_user/index.html"
+  },
+  {
+    "revision": "0d5f071ea197784bf27c",
+    "url": "/random_user/static/css/main.9b448be0.chunk.css"
+  },
+  {
+    "revision": "96bd5d985cbb368966bd",
+    "url": "/random_user/static/js/2.d95fb664.chunk.js"
+  },
+  {
+    "revision": "0d5f071ea197784bf27c",
+    "url": "/random_user/static/js/main.c43dff39.chunk.js"
+  },
+  {
+    "revision": "195917bbe886b590b1f9",
+    "url": "/random_user/static/js/runtime-main.9ac3c2f3.js"
+  }
+]);
